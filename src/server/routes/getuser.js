f0532bc72cced6681c0022545251cc824ddcd525
yushengyuan <582718643@qq.com>
2018-01-05 21:46:36 +0800

    update api

--- src/server/routes/getuser.js
+++ src/server/routes/getuser.js
@@ -4,9 +4,10 @@ const db = require('../db');
 
 /* GET users listing. */
 router.get('/getuser', (req, res, next) => {
-	db('SELECT * FROM `user_info` WHERE `ID`=0', (error, data) => {
-		data && res.json({user_data: data});
-	});
+	// db('SELECT * FROM `user_info` WHERE `ID`=0', (error, data) => {
+	//   	data && res.json({user_data: data});
+	// });
+	res.send('15107156924');
 });
 
 module.exports = router;
