f0641c7d30e1b84f4f6502d0ed0c73380c689d38
yushengyuan <yushengyuan@xmebank.com>
2018-01-05 11:50:40 +0800

    fix conflicts

--- src/client/views/mine.vue
+++ src/client/views/mine.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
     <div id="mine">
         <h1>{{greeter}}</h1>
@@ -38,44 +37,3 @@ export default {
         margin-top: 46px;
     }
 </style>
-=======
-<template>
-    <div id="mine">
-        <h1>{{greeter}}</h1>
-        <button @click="clickBtn">点击</button>
-    </div>
-</template>
-
-<script>
-export default {
-    data() {
-      return {
-        greeter: 'mine'
-      }
-    },
-    mounted: function (){
-        // alert('mine')
-    },
-    methods: {
-        clickBtn () {
-            this.$http({
-                methods: 'get',
-                url: 'http://localhost:3000/users'
-            })
-            .then( res => {
-                console.log(res)
-            })
-            .catch( err => {
-                console.log(err)
-            })
-        }
-    }
-}
-</script>
-<style scoped>
-    #mine {
-        color: #2c3e50;
-        margin-top: 46px;
-    }
-</style>
->>>>>>> 6f1d0ea58196ae288f7510b419636f2af09db505
--- src/client/views/movie.vue
+++ src/client/views/movie.vue
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 <template>
 	<div id="movie">
 		<divider>豆瓣电影top50</divider>
-		<h1>hello</h1>
 		<ul class="movie-list">
             <li v-for="item in lists" :key="item.id" @click="getWarn()">
                 <panel :header="item.type" :footer="item.footer" :list="item.list"></panel>
@@ -78,84 +76,3 @@
 		margin-top: 46px;
 	}
 </style>
-=======
-<template>
-	<div id="movie">
-		<divider>豆瓣电影top50</divider>
-		<h1>hehe</h1>
-		<ul class="movie-list">
-            <li v-for="item in lists" :key="item.id" @click="getWarn()">
-                <panel :header="item.type" :footer="item.footer" :list="item.list"></panel>
-            </li>
-        </ul>
-	</div>
-</template>
-
-<script>
-	import { Divider, Panel } from 'vux'
-
-	export default {
-		components: {
-			Divider,
-			Panel
-		},
-		data () {
-			return {
-				lists: []
-			}
-		},
-		methods: {
-			getWarn () {
-				alert(1)
-			}
-		},
-		mounted () {
-			var _this = this;
-			this.$http({
-				methods: 'post',
-				url: '/static/data/top50.json'
-			})
-			.then( (res) => {
-				res.data.subjects.forEach( (item, i) => {
-					let obj = {};
-
-					obj.type = item.title;
-
-					obj.list = [];
-					let list1 = {};
-					list1.src = item.images.medium;
-					list1.fallbackSrc = item.images.medium;
-					list1.title = item.directors[0].name;
-					list1.desc = item.title;
-					list1.url = "#";
-					obj.list.push(list1);
-
-					let meta = {};
-					meta.source = item.directors[0].name;
-					obj.list.push(meta);
-
-					obj.footer = {};
-					obj.footer.title = "查看更多";
-					obj.footer.url = "#";
-
-					_this.lists.push(obj);
-				})
-			})
-			.catch( (err) => {
-				alert(err)
-			})
-		}
-	}
-</script>
-
-<style lang="less">
-	@import '~vux/src/styles/1px.less';
-
-	.movie-list .weui-media-box_appmsg .weui-media-box__hd {
-		height: 90px;
-	}
-	#movie {
-		margin-top: 46px;
-	}
-</style>
->>>>>>> 6f1d0ea58196ae288f7510b419636f2af09db505
