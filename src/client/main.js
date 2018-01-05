cb10282b5d9ca90135b4f7506b147837e01cd330
yushengyuan <yushengyuan@xmebank.com>
2018-01-05 15:34:48 +0800

    add babel-polyfill

--- src/client/main.js
+++ src/client/main.js
@@ -2,8 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import _Promiss from 'es6-promise'
-_Promiss.polyfill()
+import 'babel-polyfill'
 import { AjaxPlugin } from 'vux'
 Vue.use(AjaxPlugin)
 import FastClick from 'fastclick'
