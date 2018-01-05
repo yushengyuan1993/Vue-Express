da6283170b623eafa72d3b87e49dd1189b5fe3e6
yushengyuan <582718643@qq.com>
2018-01-05 21:19:26 +0800

    update getuser

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db');
-
+debugger;
 /* GET users listing. */
-router.get('/users', function(req, res) {
+router.get('/users', function(req, res, next) {
   	res.send('hello yushengyuan');
 });
 
-router.post('/getuser', (req, res) => {
-	db('SELECT * FROM `user_info` WHERE `id`=0', (error, data) => {
+router.post('/getuser', (req, res, next) => {
+	db('SELECT * FROM `user_info` WHERE `ID`=0', (error, data) => {
 		data && res.json({user_data: data});
 	});
 });
