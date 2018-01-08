f20c0955791eada7eadd154d32f7094945af837f
yushengyuan <582718643@qq.com>
2018-01-08 15:24:42 +0800

    update api getuser

--- src/server/routes/getuser.js
+++ src/server/routes/getuser.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const query = require('../lib/pool');
 
 /* GET users listing. */
 router.get('/getuser', (req, res, next) => {
-	const str = query('SELECT * FROM `user_info`', (err, vals, fields) => {
-		return JSON.stringify(vals);
+	query('SELECT * FROM `user_info`', (err, vals, fields) => {
+		res.send(JSON.stringify(vals));
 	});
-	res.send(str);
 });
 
 module.exports = router;
