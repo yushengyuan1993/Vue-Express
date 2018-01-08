c1bfc4a7e6b1cfb89cedc59fbe16af4cb7ae5d8c
yushengyuan <yushengyuan@xmebank.com>
2018-01-08 16:16:43 +0800

    modify pool.js

--- src/server/lib/pool.js
+++ src/server/lib/pool.js
@@ -2,10 +2,11 @@ const mysql = require('mysql');
 
 // 创建连接池
 const pool = mysql.createPool({
-    host    : 'localhost',
-    user    : 'root',
-    password: 'yushengyuan',
-    database: 'test'
+    host           : 'localhost',
+    user           : 'root',
+    password       : 'yushengyuan',
+    database       : 'test',
+    connectionLimit: 10
 });
 
 const query = (sql, callback) => {
